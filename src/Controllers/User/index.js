96c80c355062ccaa17b9dfd6757244f3b7897932
Luis Chrestia <chrestia.luis@gmail.com>
2023-12-05 02:09:31 -0300

    user model

--- src/Controllers/User/index.js
+++ src/Controllers/User/index.js
@@ -9,3 +9,21 @@ export const postUSer = async (req, res) => {
         res.status(400).json({ message: error.message })
     }
 }
+
+export const getUser = async (req, res) => {
+    //console.log("AASDASD")
+    //console.log(req)
+    //console.log(req)
+    const { name, password } = req.query.name;
+    //console.log(name, password)
+    try {
+        const user = await User.findOne({name, password});
+        //console.log(user)
+        const userLog = {user: user.name, admin: user.admin}
+        //console.log(userLog)
+        res.status(200).json(userLog)
+    }
+    catch (error) {
+        res.status(400).json( `Usuario o contraseña incorrectos`)
+    }
+}
--- src/Routes/UserRoutes/index.js
+++ src/Routes/UserRoutes/index.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { postUSer } from '../../Controllers/User/index.js'
+import { postUSer, getUser } from '../../Controllers/User/index.js'
 
 const router = express.Router();
 
 router.post('/post', postUSer)
-// router.get('/get', getPeluTurno)
+router.get('/get', getUser)
 // router.patch('/patch/:id', patchPeluTurno)
 // router.delete('/delete/:id', deletePeluTurno)
 
