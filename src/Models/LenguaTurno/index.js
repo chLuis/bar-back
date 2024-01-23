4584d3e5127797398b4ed90777060743de3033bc
Luis Chrestia <chrestia.luis@gmail.com>
2024-01-23 13:56:40 -0300

    add name to turn

--- src/Models/LenguaTurno/index.js
+++ src/Models/LenguaTurno/index.js
@@ -2,6 +2,7 @@ import { Schema, model } from 'mongoose';
 
 const LenguaTurnoSchema = new Schema({
     idPerro: {type: String, required:[true, "id de perro es requerido"]},
+    name: {type: String, required: [true, "Nombre es requerido"]},
     date: {type: String, required: [true, "Fecha es requerido"]},
     hour: {type: String, required: [true, "Horario es requerido"]},
     typeService: {type: String, required: [true, "Tipo de servicio es requerido"]},
