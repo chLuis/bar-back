1329d806214b8eb2a88ce55f1610877192fc4918
Luis Chrestia <chrestia.luis@gmail.com>
2024-01-24 09:27:44 -0300

    fix perro for dog

--- src/Models/LenguaTurno/index.js
+++ src/Models/LenguaTurno/index.js
@@ -1,10 +1,10 @@
 import { Schema, model } from 'mongoose';
 
 const LenguaTurnoSchema = new Schema({
-    idPerro: {type: String, required:[true, "id de perro es requerido"]},
+    idDog: {type: String, required:[true, "id de perro es requerido"]},
     name: {type: String, required: [true, "Nombre es requerido"]},
     date: {type: String, required: [true, "Fecha es requerido"]},
-    hour: {type: String, required: [true, "Horario es requerido"]},
+    hour: {type: Array, required: [true, "Horario es requerido"]},
     typeService: {type: String, required: [true, "Tipo de servicio es requerido"]},
     price: {type: Number},
     payment: {type: String}
