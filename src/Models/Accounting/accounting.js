e1860d4b44ec0a7d0b46f0283bf2bbd260de3729
Luis Chrestia <chrestia.luis@gmail.com>
2024-03-14 10:36:46 -0300

    fix model accounting

--- src/Models/Accounting/accounting.js
+++ src/Models/Accounting/accounting.js
@@ -4,8 +4,24 @@ const AccountingSchema = new Schema({
     name: {type: String, required: [true, 'Name is required']},
     amount: {type: Number, required: [true, 'Amount is required']},
     category: {type: String, required: [true, 'Category is required']},
+    type: {type: String, required: [true, 'Type is required']},
     description: {type: String},
-    date: {type: Date, default: Date.now},
+    date: {
+        type: {
+            year: { type: Number, required: true },
+            month: { type: Number, required: true },
+            day: { type: Number, required: true }
+        },
+        required: [true, 'Date is required'],
+        default: () => {
+            const now = new Date();
+            return {
+                year: now.getUTCFullYear(),
+                month: now.getUTCMonth() + 1,
+                day: now.getUTCDate()
+            };
+        }
+    }
 })
 
 export default model('Accounting', AccountingSchema);
